e1fb5e6c40d3564b8f3ce176c062c3a7bb17a3df
opeyemi <solomonope@gmail.com>
2021-06-01 19:59:11 +0000

    chore: save

--- get_product_of_other_elements.go
+++ get_product_of_other_elements.go
@@ -27,7 +27,7 @@ func products(data []int) []int {
 	}
 
 	// build suffix array
-	// //edb 5ef e4f87c61  trtrtrtr9bea91a998   9002642055c   0787d
+	// //edb 5ef e4f87c61  trtrtrtrgg9bea9fff1a998   9002642055c   0787d
 	return data
 }
 func main() {
